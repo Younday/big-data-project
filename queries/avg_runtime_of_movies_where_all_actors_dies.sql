6b30d3047d39c56f68a062e5d1639101bff594ea
Henstor <hester@verdenius.com>
2021-03-25 19:06:09 +0100

    Average runtime of movies where all actors died.

---
+++ queries/avg_runtime_of_movies_where_all_actors_dies.sql
@@ -0,0 +1,36 @@
+WITH y AS (
+    WITH x AS (
+        SELECT 
+            title.principals.tconst, COUNT(nconst) AS counts
+        FROM 
+            title.principals
+        LEFT JOIN title.basics ON title.basics.tconst = title.principals.tconst
+        WHERE
+            title.principals.nconst IN (
+                SELECT 
+                    nconst
+                FROM
+                    name.basics
+                WHERE name.basics.deathYear is not Null
+        )
+            AND
+            title.basics.titleType = 'movie'
+        GROUP BY title.principals.tconst
+    )
+    SELECT tconst
+    FROM (
+        SELECT 
+            title.principals.tconst
+        FROM 
+            title.principals
+
+        JOIN x ON x.tconst = title.principals.tconst
+        GROUP BY title.principals.tconst
+        HAVING
+            x.counts = COUNT(title.principals.nconst)
+    )
+)
+SELECT
+    AVG(title.basics.runtimeMinutes) AS avg_runtime_in_minutes
+FROM y
+LEFT JOIN title.basics ON title.basics.tconst = y.tconst
