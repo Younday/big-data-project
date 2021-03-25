699b25bcb283e25e13bfa2aae5fcdc97c1cc0bf6
Sander Hansen <sanderisbestok@gmail.com>
2021-03-25 19:56:27 +0100

    Made some adjustments, read readme onhow to change data

--- README.md
+++ README.md
@@ -12,6 +12,11 @@ Run following command:
 docker-compose up -d
 ```
 
+### Data
+There are some misplaced tabs in the data, to fix this you have to use: 
+sed -i 's/"//g' title.basics.tsv
+sed -i 's/\N/0/g' title.basics.tsv
+
 ### Scripts & Queries
 
 Python scripts can be saved in scripts folder (for data loading and keeping track of runtime). Seperate SQL or Mongo queries can be kept in queries folder.
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -4,62 +4,115 @@ import time
 from tqdm import tqdm
 from pymongo import MongoClient
 import pandas as pd
+import sys
 
 # Postgres connection
-conn = psycopg2.connect("host=localhost dbname=postgres user=unicorn_user password=magical_password")
-cur = conn.cursor()
+try:
+    postgres_conn = psycopg2.connect("host=localhost dbname=postgres user=unicorn_user password=magical_password")
+    postgres_cur = postgres_conn.cursor()
+    print("Connected successfully to Postgres")
+except:
+    print("Error 1: ", sys.exc_info()[1])
 
 # MongoDB connection
 try:
-    connect = MongoClient('mongodb://root:rootpassword@localhost:27017/')
-    print("Connected successfully!!!")
+    mongo_conn = MongoClient('mongodb://root:rootpassword@localhost:27017/')
+    print("Connected successfully to MongoDB")
 except:
-    print("Could not connect to MongoDB")
+    print("Error 2: ", sys.exc_info()[1])
 
 # connecting or switching to the database
-db = connect.demoDB
+mongo_db = None
 
 
-def create_tables():
+def create_db_mongo():
+    global mongo_db
+    
+    mongo_db = mongo_conn["imdb"]
+
+def create_table_postgres():
+    # Execute the query in create_table.sql
     with open('../queries/create_table.sql','r') as inserts:
         query = inserts.read()
     try:
-        cur.execute(query)
-    except psycopg2.errors.DuplicateTable:
-        print("Table already exists in Postgres")
+        postgres_cur.execute(query)
+    except:
+       print("Error 3: ", sys.exc_info()[1])
 
 def insert_data_mongo():
-    collection = db.title_basics
+    collection = mongo_db.title_basics 
+    
+    # Read csv to dataframe and convert to dict (json like)
     df = pd.read_csv("../data/title.basics.tsv",sep='\t')
     mongo_records = df.to_dict('records')
+    
+    # Start timer
     t0 = time.time()
-    collection.insert_many(mongo_records)
+    
+    try:
+        # Json to database
+        collection.insert_many(mongo_records)
+    except:
+        print("Error 4:", sys.exc_info()[1])
+    
+    # End timer
     t1 = time.time()
+    
     return t1-t0
 
-def read_data():
+def insert_data_postgres():
+    tsv_file = open("../data/title.basics.tsv", 'r')
 
-    tsv_file = open("../data/title.basics.tsv")
-    read_tsv = csv.reader(tsv_file, delimiter="\t")
+    # Skip header file
+    next(tsv_file)
 
+    # Start timer
     t0 = time.time()
-    next(read_tsv) # Skip the header row.
-    for row in tqdm(read_tsv):
-        if row[7] == '\\N':
-            row[7] = 0
-        cur.execute(
-    "INSERT INTO title_basics VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
-        row)
-    conn.commit()
+
+    try:
+        # File to database
+        postgres_cur.copy_from(tsv_file, 'title_basics', sep="\t")
+        postgres_conn.commit()
+    except:
+        print("Error 5: ", sys.exc_info()[1])
+
+    # End timer
     t1 = time.time()
+    
     return t1-t0
+
+def clear_mongo():
+    try:
+        mongo_conn.drop_database('imdb')
+    except:
+        print("Error 6:", sys.exc_info()[1])
+
+def clear_postgres():
+    with open('../queries/drop_table.sql','r') as inserts:
+        query = inserts.read()
+    try:
+        postgres_cur.execute(query)
+        rows = postgres_cur.fetchall()
+        for row in rows:
+            postgres_cur.execute("drop table " + row[1] + " cascade")
+    except:
+        print("Error 7: ", sys.exc_info()[1])
+
+
 
 def main():
-    create_tables()
-    total_postgres = read_data()
+    clear_postgres()
+    clear_mongo()
+    
+    create_table_postgres()
+    create_db_mongo()
+
+    total_postgres = insert_data_postgres()
     total_mongo = insert_data_mongo()
-    print("Total time loading title.basics table for Postgres: %s", total_postgres)
-    print("Total time loading title.basics table for MongoDB: %s", total_mongo)
+    print("Total time loading title.basics table for Postgres: ", total_postgres)
+    print("Total time loading title.basics table for MongoDB: ", total_mongo)
+
+
 
 if __name__ == "__main__":
     main()
